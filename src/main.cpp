b627864069c1a5fcbb5bc00e58c70325acfc5fdc
graffy <0532977@schoolsnet.act.edu.au>
2026-07-23 10:43:37 +1000

    basic movement stuff

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,29 @@
 #include <ultrasonic.h>
 #include <ESP32Servo.h>
 
+#define leftLEDPin 2
+#define rightLEDPin 12
+#define antiClockwise Contrarotate
 vehicle myCar;
 
 
 void setup()
 {
- 
+  Serial.begin(115200);
+  myCar.Init();
+  pinMode(leftLEDPin, OUTPUT);
+  pinMode(rightLEDPin, OUTPUT);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  digitalWrite(rightLEDPin, LOW);
+  digitalWrite(leftLEDPin, HIGH);
+  myCar.Move(Move_Right, 255);
+  delay(1000);
+  digitalWrite(rightLEDPin, HIGH);
+  digitalWrite(leftLEDPin, LOW);
+  myCar.Move(Move_Left,255);
 
-  
+  delay(1000);
 }
