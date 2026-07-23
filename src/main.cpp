e0bb27f3cf28675b3690efb6c8f977eeba33fff8
graffy <0532977@schoolsnet.act.edu.au>
2026-07-23 11:00:10 +1000

    servo setup and buzzer bs

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,15 @@
 
 #define leftLEDPin 2
 #define rightLEDPin 12
+#define buzzerPin 33
 #define antiClockwise Contrarotate
 vehicle myCar;
 
+// MOTOR TERMS (USE INSIDE OF "myCar.Move()")!!
+//   Forward & Backward
+//   Clockwise & antiClockwise
+//   Move_Left & Move_Right
+//   Stop
 
 void setup()
 {
@@ -21,11 +27,13 @@ void loop()
 {
   digitalWrite(rightLEDPin, LOW);
   digitalWrite(leftLEDPin, HIGH);
-  myCar.Move(Move_Right, 255);
+ // myCar.Move(Move_Right, 255);
+  tone(buzzerPin, 262);
   delay(1000);
   digitalWrite(rightLEDPin, HIGH);
   digitalWrite(leftLEDPin, LOW);
-  myCar.Move(Move_Left,255);
+ // myCar.Move(Move_Left,255);
+  noTone(buzzerPin);
 
   delay(1000);
 }
