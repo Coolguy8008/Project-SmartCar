1a00e2b8f5a4b66fc095689e4376658436e23d3c
graffy <0532977@schoolsnet.act.edu.au>
2026-08-04 12:00:16 +1000

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,12 @@ vehicle myCar;
 ultrasonic myUltrasonic;
 Servo myServo;
 
-int UT_ditance = 0;
-// MOTOR TERMS (USE INSIDE OF "myCar.Move()")!!
-//   Forward & Backward
-//   Clockwise & antiClockwise
-//   Move_Left & Move_Right
-//   Stop
+int UT_distance = 0;
+// MOTOR TERMS (USE INSIDE OF "myCar.Move()") speed is out of 255
+//   Forward, [speed] & Backward [speed]
+//   Clockwise, [speed] & antiClockwise, [speed]
+//   Move_Left, [speed] & Move_Right, [speed]
+//   Stop, [speed]
 
 void setup()
 {
@@ -32,10 +32,34 @@ void setup()
   pinMode(rightLEDPin, OUTPUT);
 }
 
+void checkDistance()
+{
+  if (UT_distance > 60)
+  {
+    myCar.Move(Forward, 200);
+    tone(buzzerPin, 0);
+  }
+  else
+  {
+    if (UT_distance < 40)
+    {
+      myCar.Move(Backward, 200);
+      tone(buzzerPin, 0);
+    }
+    else
+    {
+      myCar.Move(Stop, 0);
+      tone(buzzerPin, 262);
+    }
+  }
+}
+
 void loop()
 {
-  UT_ditance = myUltrasonic.Ranging();
-  Serial.print(UT_ditance);
-  Serial.println("cm");
-  delay(1000);
+  UT_distance = myUltrasonic.Ranging();
+  checkDistance();
+  myServo.write(90);
+  // Serial.print(UT_distance);
+  // Serial.println("cm");
+  delay(250);
 }
