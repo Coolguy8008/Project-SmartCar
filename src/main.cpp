610a0fea18453779ba10891c8ea61c6b21d77a82
graffy <0532977@schoolsnet.act.edu.au>
2026-08-11 11:59:20 +1000

    Ir functionality added

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,159 @@
-// #include <IRremote.h>
-// #include <Arduino.h>
-// #include <vehicle.h>
-// #include <ultrasonic.h>
-// #include <ESP32Servo.h>
-
-// #define IRpin 4
-// #define leftLEDPin 2
-// #define rightLEDPin 12
-// #define buzzerPin 33
-// #define servoPin 25
-// #define sonarPin 14
-// #define echoPin 15
-// #define antiClockwise Contrarotate
-
-// IRrecv myIRrecv(IRpin);
-// vehicle myCar;
-// ultrasonic myUltrasonic;
-// Servo myServo;
-
-// int UT_distance = 0;
-// // MOTOR TERMS (USE INSIDE OF "myCar.Move()") speed is out of 255
-// //   Forward, [speed] & Backward [speed]
-// //   Clockwise, [speed] & antiClockwise, [speed]
-// //   Move_Left, [speed] & Move_Right, [speed]
-// //   Stop, [speed]
-
-// void setup()
-// {
-//   Serial.begin(115200);
-//   myIRrecv.enableIRIn();
-//   myCar.Init();
-//   myServo.attach(servoPin);
-//   myServo.write(90);
-//   myUltrasonic.Init(13, 14);
-//   pinMode(leftLEDPin, OUTPUT);
-//   pinMode(rightLEDPin, OUTPUT);
-// }
-
-// void checkDistance()
-// {
-//   if (UT_distance > 60)
-//   {
-//     myCar.Move(Forward, 200);
-//    // tone(buzzerPin, 0);
-//   }
-//   else
-//   {
-//     if (UT_distance < 40)
-//     {
-//       myCar.Move(Backward, 200);
-//      // tone(buzzerPin, 0);
-//     }
-//     else
-//     {
-//       myCar.Move(Stop, 0);
-//     //  tone(buzzerPin, 262);
-//     }
-//   }
-// }
-
-// void loop()
-// {
-//   UT_distance = myUltrasonic.Ranging();
-//  // checkDistance();
-//   if(myIRrecv.decode()){
-//     Serial.println(myIRrecv.decodedIRData.decodedRawData,HEX);
-//     myIRrecv.resume();
-//   }
-//   // Serial.print(UT_distance);
-//   // Serial.println("cm");
-//   delay(250);
-// }
-
 #include <IRremote.h>
-#define IRpin 4         // Declare the pin of the infrared receiver
-IRrecv myIRrecv(IRpin); // Creating an infrared receiver object
+#include <Arduino.h>
+#include <vehicle.h>
+#include <ultrasonic.h>
+#include <ESP32Servo.h>
+
+#define IRpin 4
+#define leftLEDPin 2
+#define rightLEDPin 12
+#define buzzerPin 33
+#define servoPin 25
+#define sonarPin 14
+#define echoPin 15
+#define antiClockwise Contrarotate
+
+IRrecv myIRrecv(IRpin);
+vehicle myCar;
+ultrasonic myUltrasonic;
+Servo myServo;
+
+unsigned long lastCommandTime;            // Record the time of the last received command
+const unsigned long commandTimeout = 100; // Set the timeout period (milliseconds)
+uint32_t last_decode = 0;                 // Variable to store the previously decoded raw data
+uint32_t current_decode = 0;              // Variable to store the currently decoded raw data
+
+int SPEED = 200;
+
+int UT_distance = 0;
+// MOTOR TERMS (USE INSIDE OF "myCar.Move()") speed is out of 255
+//   Forward, [speed] & Backward [speed]
+//   Clockwise, [speed] & antiClockwise, [speed]
+//   Move_Left, [speed] & Move_Right, [speed]
+//   Stop, [speed]
+
 void setup()
 {
-  Serial.begin(9600);
-  myIRrecv.enableIRIn(); // Start receiving infrared
+  Serial.begin(115200);
+  myIRrecv.enableIRIn();
+  myCar.Init();
+  myServo.attach(servoPin);
+  myServo.write(90);
+  myUltrasonic.Init(13, 14);
+  pinMode(leftLEDPin, OUTPUT);
+  pinMode(rightLEDPin, OUTPUT);
 }
-void loop()
+
+// IR BUTTON FUNCTIONS
+void arrowLeft()
+{
+  myCar.Move(antiClockwise, SPEED);
+}
+
+void arrowRight()
+{
+  myCar.Move(Clockwise, SPEED);
+}
+
+void arrowUp()
+{
+  myCar.Move(Forward, SPEED);
+}
+
+void arrowDown()
+{
+  myCar.Move(Backward, SPEED);
+}
+
+void but1()
+{
+  myCar.Move(Move_Left, SPEED);
+}
+
+void but3()
+{
+  myCar.Move(Move_Right, SPEED);
+}
+
+// FOLLOW FUNCTIONS 
+
+void checkDistance()
+{
+  if (UT_distance > 60)
+  {
+    myCar.Move(Forward, SPEED);
+    // tone(buzzerPin, 0);
+  }
+  else
+  {
+    if (UT_distance < 40)
+    {
+      myCar.Move(Backward, SPEED);
+      // tone(buzzerPin, 0);
+    }
+    else
+    {
+      myCar.Move(Stop, 0);
+      //  tone(buzzerPin, 262);
+    }
+  }
+}
+
+// GENERAL CONTROL
+
+void handleIRrecieve()
 {
   if (myIRrecv.decode())
-  { // Whether infrared is received
-    // Print infrared decoding values
-    Serial.println(myIRrecv.decodedIRData.decodedRawData, HEX);
-    myIRrecv.resume(); // Wait for the next infrared reading
+  {                             // Check if an IR signal is received
+    lastCommandTime = millis(); // Update the last command time
+    current_decode = myIRrecv.decodedIRData.decodedRawData;
+    if (myIRrecv.decodedIRData.flags)
+    { // Check if it's a repeated IR code
+      current_decode = last_decode;
+      // Set current decode as the previous one
+    }
+    Serial.print(current_decode, HEX);
+    Serial.println("");
+    switch (current_decode)
+    {
+    case 0xB946FF00:
+      arrowUp();
+      break;
+
+    case 0xEA15FF00:
+      arrowDown();
+      break;
+
+    case 0xBB44FF00:
+      arrowLeft();
+      break;
+    case 0xBC43FF00:
+      arrowRight();
+      break;
+
+    case 0xE916FF00:
+      but1();
+      break;
+
+    case 0xF20DFF00:
+      but3();
+      break;
+
+    }
+    last_decode = current_decode;
+    // Update the stored previous decodedRawData
+    myIRrecv.resume(); // Wait for the next IR signal
   }
-  delay(100);
+  if (millis() - lastCommandTime > commandTimeout)
+  {
+    myCar.Move(Stop, 0);
+    // If no new IR signal within 100 milliseconds, stop the smart car
+  }
+}
+
+void loop()
+{
+  UT_distance = myUltrasonic.Ranging();
+  handleIRrecieve();
+  delay(250);
 }
