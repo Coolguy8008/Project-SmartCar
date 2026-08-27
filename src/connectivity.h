1319abbcefa51ce0c596d3c8ed9438fd7fc359b9
graffy <0532977@schoolsnet.act.edu.au>
2026-08-27 11:06:27 +1000

    starting idle simple
    delay time not working >:(

--- src/connectivity.h
+++ src/connectivity.h
@@ -13,6 +13,3 @@ void setupWiFi() {
     Serial.print(".");
   }
 }
-
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,40 @@
 #include <IRremote.h>
 #include <Arduino.h>
-#include <vehicle.h>
-#include <ultrasonic.h>
-#include <ESP32Servo.h>
-#include <IRfunctions.h>
+#include <behaviours.h>
 
-#define IRpin 4
-#define leftLEDPin 2
-#define rightLEDPin 12
-#define buzzerPin 33
-#define servoPin 25
-#define sonarPin 14
-#define echoPin 15
-
-// FACE OBJECT VARIABLES
-int leftSide;
-int rightSide;
-int target;
-bool facingTarget = false;
-
-IRrecv myIRrecv(IRpin);
-ultrasonic myUltrasonic;
-Servo myServo;
-
+typedef void (*FuncPtr)();
+int actionTime = random(2,5);
 unsigned long lastCommandTime;            // Record the time of the last received command
 const unsigned long commandTimeout = 100; // Set the timeout period (milliseconds)
 uint32_t last_decode = 0;                 // Variable to store the previously decoded raw data
 uint32_t current_decode = 0;              // Variable to store the currently decoded raw data
 
-int UT_distance = 0;
-bool recieveInstruction = true;
+// IDLE SETUP
+void action0()
+{
+ Serial.println("0");
+  myCar.Move(Forward, SPEED);
+  delay(actionTime);
+  myCar.Move(Stop, 0);
+  Serial.println("0 a");
+}
+void action1()
+{
+  myCar.Move(Backward, SPEED);
+ // delay(actionTime);
+  //myCar.Move(Stop, 0);
+  Serial.println("1");
+}
+void action2() { Serial.println("2"); }
+void action3() { Serial.println("3"); }
+void action4() { Serial.println("4"); }
+void action5() { Serial.println("5"); }
+void action6() { Serial.println("6"); }
+void action7() { Serial.println("7"); }
+
+FuncPtr actions[20] = {
+    action0, action1, action2, action3, action4, action5, action6, action7};
+
 // MOTOR TERMS (USE INSIDE OF "myCar.Move()") speed is out of 255
 //   Forward, [speed] & Backward [speed]
 //   Clockwise, [speed] & antiClockwise, [speed]
@@ -47,166 +52,143 @@ void setup()
   myServo.attach(servoPin);
   myServo.write(90);
   myUltrasonic.Init(13, 14);
+  randomSeed(analogRead(0));
   pinMode(leftLEDPin, OUTPUT);
   pinMode(rightLEDPin, OUTPUT);
+  pinMode(IrLeft, INPUT);
+  pinMode(IrRight, INPUT);
 }
-void findLeft()
-{
-  while (facingTarget == false)
-  {
 
-    UT_distance = myUltrasonic.Ranging();
-    myCar.Move(antiClockwise, 200);
-    Serial.println(UT_distance);
-    if (target - 3 <= UT_distance && target + 3 >= UT_distance)
-    {
-      Serial.println("FOUND");
-      facingTarget = true;
-    }
-    delay(100);
-  }
-  // delay(500);
-  myCar.Move(Stop, 0);
-}
-void findRight()
+void reset()
 {
-  while (facingTarget == false)
-  {
-
-    UT_distance = myUltrasonic.Ranging();
-    myCar.Move(Clockwise, 200);
-    Serial.println(UT_distance);
-    if (target - 3 <= UT_distance && target + 3 >= UT_distance)
-    {
-      Serial.println("FOUND");
-      facingTarget = true;
-    }
-    delay(100);
-  }
-  // delay(500);
+  leftSide = 0;
+  rightSide = 0;
+  target = 0;
+  facingTarget = false;
+  UT_distance = 0;
+  recieveInstruction = true;
+  followState = false;
+  manual = false;
+  leftIR = 0;
+  rightIR = 0;
+  SPEED = 200;
+  myServo.write(90);
   myCar.Move(Stop, 0);
 }
 
-void findClosest()
-{
-  if ((followState == false) & (manual == false))
-  {
-    // FIND RIGHT DISTANCE
-    recieveInstruction = false;
-    myServo.write(0);
-    delay(300);
-    rightSide = myUltrasonic.Ranging();
-    Serial.print(rightSide);
-    Serial.println(" RIGHT");
-    // FIND LEFT SIDE
-    myServo.write(180);
-    delay(400);
-    leftSide = myUltrasonic.Ranging();
-    Serial.print(leftSide);
-    Serial.println(" LEFT");
-    // FIND LOWEST
-    target = min(leftSide, rightSide);
-    myServo.write(90);
-    delay(400);
-    Serial.print("target: ");
-    Serial.println(target);
-    delay(500);
-    // CHOOSE FACE SIDE
-    if (target < 100)
-    {
-      if (target == leftSide)
-      {
-        findLeft();
-      }
-      else
-      {
-        findRight();
-      }
-    }
-    // RESET
-    leftSide = 0;
-    rightSide = 0;
-    recieveInstruction = true;
-    facingTarget = false;
-  }
-}
 // IR BUTTON FUNCTIONS
 
 void handleIRrecieve()
 {
-  if (recieveInstruction)
-  {
-    if (myIRrecv.decode())
-    {                             // Check if an IR signal is received
-      lastCommandTime = millis(); // Update the last command time
-      current_decode = myIRrecv.decodedIRData.decodedRawData;
-      if (myIRrecv.decodedIRData.flags)
-      { // Check if it's a repeated IR code
-        current_decode = last_decode;
-        // Set current decode as the previous one
-      }
-      Serial.print(current_decode, HEX);
-      Serial.println("");
-      switch (current_decode)
-      {
-      case 0xB946FF00:
-        arrowUp();
-        break;
-
-      case 0xEA15FF00:
-        arrowDown();
-        break;
-
-      case 0xBB44FF00:
-        arrowLeft();
-        break;
-      case 0xBC43FF00:
-        arrowRight();
-        break;
-
-      case 0xE916FF00:
-        but1();
-        break;
-
-      case 0xF20DFF00:
-        but3();
-        break;
-
-      case 0xE619FF00:
-        but2();
-        break;
-
-      case 0xBD42FF00:
-        findClosest();
-        break;
-
-      case 0xB54AFF00:
-        toggleFollow();
-        break;
-
-      case 0xBF40FF00:
-        toggleManual();
-        break;
-
-      case 0xF708FF00:
-        speedUp();
-        break;
-
-      case 0xA55AFF00:
-        speedDown();
-        break;
-      }
-      last_decode = current_decode;
-      // Update the stored previous decodedRawData
-      myIRrecv.resume(); // Wait for the next IR signal
+  if (myIRrecv.decode())
+  {                             // Check if an IR signal is received
+    lastCommandTime = millis(); // Update the last command time
+    current_decode = myIRrecv.decodedIRData.decodedRawData;
+    if (myIRrecv.decodedIRData.flags)
+    { // Check if it's a repeated IR code
+      current_decode = last_decode;
+      // Set current decode as the previous one
     }
-    if ((millis() - lastCommandTime > commandTimeout) && (manual))
+    Serial.print(current_decode, HEX);
+    Serial.println("");
+    switch (current_decode)
     {
-      myCar.Move(Stop, 0);
-      // If no new IR signal within 100 milliseconds, stop the smart car
+    case 0xB946FF00:
+      if (recieveInstruction)
+      {
+        arrowUp();
+      }
+      break;
+
+    case 0xEA15FF00:
+      if (recieveInstruction)
+      {
+        arrowDown();
+      }
+      break;
+
+    case 0xBB44FF00:
+      if (recieveInstruction)
+      {
+        arrowLeft();
+      }
+      break;
+    case 0xBC43FF00:
+      if (recieveInstruction)
+      {
+        arrowRight();
+      }
+      break;
+
+    case 0xE916FF00:
+      if (recieveInstruction)
+      {
+        but1();
+      }
+      break;
+
+    case 0xF20DFF00:
+      if (recieveInstruction)
+      {
+        but3();
+      }
+      break;
+
+    case 0xE619FF00:
+      if (recieveInstruction)
+      {
+        but2();
+      }
+      break;
+
+    case 0xBD42FF00:
+      if (recieveInstruction)
+      {
+        findClosest();
+      }
+      break;
+
+    case 0xB54AFF00:
+      if (recieveInstruction)
+      {
+        toggleFollow();
+      }
+      break;
+
+    case 0xBF40FF00:
+      if (recieveInstruction)
+      {
+        toggleManual();
+      }
+      break;
+
+    case 0xF708FF00:
+      if (recieveInstruction)
+      {
+        speedDown();
+      }
+      break;
+
+    case 0xA55AFF00:
+      if (recieveInstruction)
+      {
+        speedUp();
+      }
+      break;
+    case 0xAD52FF00:
+      reset();
     }
+    last_decode = current_decode;
+    // Update the stored previous decodedRawData
+    myIRrecv.resume(); // Wait for the next IR signal
+  }
+  if ((millis() - lastCommandTime > commandTimeout) && (manual))
+  {
+    myCar.Move(Stop, 0);
+    // If no new IR signal within 100 milliseconds, stop the smart car
   }
 }
+
 void ledController()
 {
   digitalWrite(leftLEDPin, manual);
@@ -238,12 +220,22 @@ void checkDistance()
   }
 }
 
+void updateSensors()
+{
+  rightIR = analogRead(IrLeft);
+  leftIR = analogRead(IrRight);
+  UT_distance = myUltrasonic.Ranging();
+}
+
 void loop()
 {
-  UT_distance = myUltrasonic.Ranging();
+  actionTime = random(2, 5);
+  Serial.println(actionTime);
+  int choice = random(0, 7); // Generates number 0 to 19
+  actions[choice]();         // Call selected function
+  updateSensors();
   checkDistance();
   handleIRrecieve();
   ledController();
-  Serial.println(SPEED);
-  delay(250);
+  delay(1000);
 }
